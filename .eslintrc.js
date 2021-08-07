35280536ecbf3a3d24ed55b0b987cb871bfb367c
titanajinurceha <50227788+titanajinurceha@users.noreply.github.com>
2021-08-07 15:39:18 +0700

    Updated file for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,17 @@
 module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
     env: {
-        "browser": true
+      browser: true,
+      es2021: true,
+      node: true,
     },
-    plugin: ["react"],
-    rules: {
-        "react/no-set-state": "off",
-        // enable additional rules
-        "indent": ["error", 4],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-
-        // override configuration set by extending "eslint:recommended"
-        "no-empty": "warn",
-        "no-cond-assign": ["error", "always"],
-
-        // disable rules from base configurations
-        "for-direction": "off"
-    }
-};
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {},
+  };
